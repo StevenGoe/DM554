6596a685ff427c45e8df378a401ac1cc585bd019
Peter Severin Rasmussen <git@petersr.com>
2015-03-06 15:04:24 +0100

    Added a main python file where the scripts and calculations should be written. Also updated gitignore to ignore the document folder generated by the latex module.

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from latex import *
+
+doc = LatexDocument("document")
+
+
+# Your script and calculations here
+
+
+doc.compile() # Generate and compile a tex file in a folder
+#doc.to_file() # Only generate
